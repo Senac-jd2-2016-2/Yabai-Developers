5e61a9840165755d379f1f1e6f1d85ab0b23d385
Lukets402 <lucas-adriano-1@hotmail.com>
2016-03-01 07:57:24 -0300

    comite inicial

--- Cooperation_Pixel/Cooperation_Pixel/Game1.cs
+++ Cooperation_Pixel/Cooperation_Pixel/Game1.cs
@@ -4,13 +4,20 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Cooperation_Pixel
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        Texture2D Background_img;
+        Rectangle Background;
+
+        Texture2D jogador_img;
+        Rectangle jogador;
+
+        private personagem humano = new personagem(150, 100);
+
 
         public Game1()
         {
@@ -18,64 +25,73 @@ namespace Cooperation_Pixel
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            Background = new Rectangle(0, 0, Window.ClientBounds.Width, Window.ClientBounds.Height);
+            jogador = new Rectangle(humano.x, humano.y, 50, 40);
+
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            Background_img = Content.Load<Texture2D>("img_pixel");
+            jogador_img = Content.Load<Texture2D>("norton");
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+            Keys[] keys = Keyboard.GetState().GetPressedKeys();
+            foreach(Keys k in keys)
+            {
+                if (k.Equals(Keys.Up) && (humano.y > 0))
+                {
+                    humano.moverY(-2);
+                }
+                if (k.Equals(Keys.Down) && (humano.y < Window.ClientBounds.Height-210))
+                {
+                    humano.moverY(2);
+                }
+                if (k.Equals(Keys.Right) && (humano.x < Window.ClientBounds.Width-130))
+                {
+                    humano.moverX(2);
+                }
+                if (k.Equals(Keys.Left) && (humano.x >= 0))
+                {
+                    humano.moverX(-2);
+                }
+            }
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(Background_img, Background, Color.White);
+            spriteBatch.Draw(jogador_img, humano.getVector(), Color.White);
+
+            spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
